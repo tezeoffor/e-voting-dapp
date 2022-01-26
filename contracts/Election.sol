0d49be470f78b228d03751c6f84b0667cc5935d4
TG <tezeoffor@gmail.com>
2022-01-26 13:53:55 +0100

    initial setup

---
+++ contracts/Election.sol
@@ -0,0 +1,9 @@
+pragma solidity >=0.4.21 <0.6.0;
+
+contract Election {
+    //Structure of candidate standing in the election
+
+    //Storing candidates in a map
+
+    //Number of candidates in standing in the election
+}
