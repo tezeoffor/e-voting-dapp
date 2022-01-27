3f33c4fa7c672629dc5271af79a2c164bd2a84aa
TG <tezeoffor@gmail.com>
2022-01-27 15:45:10 +0100

    fixed solidity version error

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,9 +1,9 @@
-pragma solidity >=0.4.21 <0.6.0;
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.6.0 <0.9.0;
 
 contract Election {
     //Structure of candidate standing in the election
-
     //Storing candidates in a map
-
     //Number of candidates in standing in the election
 }
