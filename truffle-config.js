8c3dd24266d53ba06c89b9c06095f9fbdb73aeee
TG <tezeoffor@gmail.com>
2022-06-30 20:31:08 +0100

    server side code

--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require("dotenv").config();
 const HDWalletProvider = require("@truffle/hdwallet-provider");
 
 //Account credentials from which our contract will be deployed
@@ -7,22 +7,43 @@ const mnemonic = process.env.MNEMONIC;
 //API key of your Datahub account for Avalanche Fuji test network
 const APIKEY = process.env.APIKEY;
 
+const projectId = process.env.projectId;
+
 module.exports = {
   networks: {
     fuji: {
-      provider: function() {
-            return new HDWalletProvider({mnemonic, providerOrUrl: `http://avalanche--fuji--rpc.datahub.figment.io/apikey/${APIKEY}/ext/bc/C/rpc`, chainId: "0xa869"})
+      provider: function () {
+        return new HDWalletProvider({
+          mnemonic,
+          providerOrUrl: `https://avalanche--fuji--rpc.datahub.figment.io/apikey/${APIKEY}`,
+
+          chainId: "0xa869",
+        });
       },
       network_id: "*",
       gas: 3000000,
       gasPrice: 470000000000,
-      skipDryRun: true
-    }
+      skipDryRun: true,
+      networkCheckTimeoutnetworkCheckTimeout: 10000,
+      timeoutBlocks: 200,
+    },
+    ropsten: {
+      provider: () =>
+        new HDWalletProvider(
+          mnemonic,
+          `https://ropsten.infura.io/v3/${projectId}`
+        ),
+      network_id: 3, // Ropsten's id
+      gas: 5500000, // Ropsten has a lower block limit than mainnet
+      confirmations: 2, // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true, // Skip dry run before migrations? (default: false for public nets )
+    },
   },
   solc: {
     optimizer: {
       enabled: true,
-      runs: 200
-    }
-  }
-}
+      runs: 200,
+    },
+  },
+};
